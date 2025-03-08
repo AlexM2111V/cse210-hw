0f664f95b2f6d6f2e6246398245869126b9696ed
AlexM2111V <jesusmendozav2111@gmail.com>
2025-03-07 20:30:55 -0500

    Exercise 2 was cmopleted

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int gradePercentage = int.Parse(percentage);
+
+        string letter;
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80 && gradePercentage < 90)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70 && gradePercentage < 80)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60 && gradePercentage < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You almost did it. Do not give up!");
+        }
     }
 }
