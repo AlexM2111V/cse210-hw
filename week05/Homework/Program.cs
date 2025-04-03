c95b2a023cb0bcdcfbec97b4f0365ad9b68d1db9
AlexM2111V <jesusmendozav2111@gmail.com>
2025-04-02 22:21:01 -0500

    W05 Learning Activity: Inheritance added

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Josh Carter", "Math II");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Linda Wilson", "Profesional Skills", "8.9", "10-18");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Alexander Mendoza", "Programming with classes", "Abstraction");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
