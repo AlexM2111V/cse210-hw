0d4a3e240e757818a4921c4bb1fad4f9e5057351
AlexM2111V <jesusmendozav2111@gmail.com>
2025-04-10 23:05:10 -0500

    Shapes program added

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("Red", 12.5);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("Brown", 13.4, 10);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("Blue", 13.2);
+        shapes.Add(circle1);        
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");    
+        }
     }
 }
